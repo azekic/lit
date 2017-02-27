5123c186ffbff1f414a59df5d96fbacc02211172
andre <azekic@uwo.ca>
2017-02-27 17:40:24 -0500

    test

---
+++ angularregistration/js/controllers/events.js
@@ -0,0 +1,38 @@
+myApp.controller('EventsController',
+  ['$scope', '$rootScope','$firebaseAuth', '$firebaseArray',
+  function($scope, $rootScope, $firebaseAuth, $firebaseArray) {
+
+    var ref = firebase.database().ref();
+    var auth = $firebaseAuth();
+
+    auth.$onAuthStateChanged(function(authUser) {
+      if(authUser) {
+        var eventsRef = ref.child('users').child(authUser.uid).child('events');
+        var eventsInfo = $firebaseArray(eventsRef);
+
+        $scope.events = eventsInfo;
+
+        eventsInfo.$loaded().then(function(data) {
+          $rootScope.howManyEvents = eventsInfo.length;
+        }); // make sure event data is loaded
+
+        eventsInfo.$watch(function(data) {
+          $rootScope.howManyEvents = eventsInfo.length;
+        });
+
+        $scope.addEvent = function() {
+          eventsInfo.$add({
+            name: $scope.eventname,
+            date: firebase.database.ServerValue.TIMESTAMP
+          }).then(function() {
+            $scope.eventname='';
+          }); //promise
+        } //addEvent
+
+        $scope.deleteEvent = function(key) {
+          eventsInfo.$remove(key);
+        } //deleteEvent
+
+      } //authUser
+    }); //onAuthStateChanged
+}]); //myApp.controller
