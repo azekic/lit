4ae126dba2911ba393173c4e411bfa5908a93df3
Garren27 <gmccall5@uwo.ca>
2017-03-03 20:08:48 -0500

    Create allevents.js

---
+++ angularregistration/js/controllers/allevents.js
@@ -0,0 +1,50 @@
+/**
+ * Created by Garren on 2017-03-01.
+ */
+myApp.controller('AllEventsController',
+    ['$scope', '$rootScope','$firebaseAuth', '$firebaseArray',
+        function($scope, $rootScope, $firebaseAuth, $firebaseArray){
+
+    var ref = firebase.database().ref();
+    var auth = $firebaseAuth();
+            auth.$onAuthStateChanged(function(authUser) {
+                if(authUser) {
+                    var eventsRef = ref.child('users').child(authUser.uid).child('events');
+                    var eventsInfo = $firebaseArray(eventsRef);
+
+                    var timelineRef = ref.child('events');
+                    var timelineInfo = $firebaseArray(timelineRef);
+
+                    $scope.events = eventsInfo;
+
+                    timelineInfo.$loaded().then(function(data) {
+                        $rootScope.howManyEvents = timelineInfo.length;
+                    }); // make sure event data is loaded
+
+                    timelineInfo.$watch(function(data) {
+                        $rootScope.howManyEvents = timelineInfo.length;
+                    });
+
+                    $scope.addEvent = function() {
+                        eventsInfo.$add({
+                            name: $scope.eventname,
+                            date: firebase.database.ServerValue.TIMESTAMP
+                        }).then(function() {
+                            $scope.eventname='';
+                        }); //promise
+
+                        timelineInfo.$add({
+                            name: $scope.eventname,
+                            date: firebase.database.ServerValue.TIMESTAMP
+                        }).then(function() {
+                            $scope.eventname='';
+                        }); //promise
+                    } //addEvent
+
+                    $scope.deleteEvent = function(key) {
+                        eventsInfo.$remove(key);
+                    } //deleteEvent
+
+                } //authUser
+            }); //onAuthStateChanged
+}]);
