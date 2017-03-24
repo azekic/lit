00bcd8db1b0551d02f107fb75d3fe65d3fd90374
Mitch <mmancus4@uwo.ca>
2017-03-24 14:57:55 -0400

    Edit Events Now Working

--- js/controllers/eventedit.js
+++ js/controllers/eventedit.js
@@ -1,33 +1,33 @@
 myApp.controller('EventEditController',
-  ['$scope', '$rootScope', '$location', '$routeParams', '$firebaseObject', '$firebaseArray',
-  function($scope, $rootScope, $location, $routeParams, $firebaseObject, $firebaseArray)  {
-    $scope.whichevent = $routeParams.mId;
-    $scope.whichuser = $routeParams.uId;
-    var refEvent = firebase.database().ref('users/'+$scope.whichuser+
-        '/events/'+$scope.whichevent);
-    var ref = firebase.database().ref();
+    ['$scope', '$rootScope', '$location', '$routeParams', '$firebaseObject', '$firebaseArray',
+        function($scope, $rootScope, $location, $routeParams, $firebaseObject, $firebaseArray)  {
+            $scope.whichevent = $routeParams.mId;
+            $scope.whichuser = $routeParams.uId;
+            var refEvent = firebase.database().ref('users/'+$scope.whichuser+
+                '/events/'+$scope.whichevent);
+            var ref = firebase.database().ref();
 
-    $scope.editEvent = function() {
-          refEvent.update({
-            eventname: $scope.eventname,
-            eventdate: $scope.eventdate,
-            eventtype: $scope.eventtype,
-            eventfloor: $scope.eventfloor,
-            eventdescription: $scope.eventdescription,
-            archive: "false",
-          }).then(function() {
-            var regEventRef = ref.child('events').child($scope.whichevent).update({
-              eventname: $scope.eventname,
-              eventdate: $scope.eventdate,
-              eventtype: $scope.eventtype,
-              eventfloor: $scope.eventfloor,
-              eventdescription: $scope.eventdescription,
-            }); //This little part creates the same event id in the Events part of the database
-            $scope.eventname='';//empties input field
-            $scope.eventdate='';//empties input field
-            $scope.eventtype='';//empties input field
-            window.location.href = "/app/#/events";
-          }); //promise
-      } //editEvent
+            $scope.editEvent = function() {
+                refEvent.update({
+                    eventname: $scope.eventname,
+                    eventdate: $scope.eventdate,
+                    eventtype: $scope.eventtype,
+                    eventfloor: $scope.eventfloor,
+                    eventdescription: $scope.eventdescription,
+                    archive: "false",
+                }).then(function() {
+                    var regEventRef = ref.child('events').child($scope.whichevent).update({
+                        eventname: $scope.eventname,
+                        eventdate: $scope.eventdate,
+                        eventtype: $scope.eventtype,
+                        eventfloor: $scope.eventfloor,
+                        eventdescription: $scope.eventdescription,
+                    }); //This little part creates the same event id in the Events part of the database
+                    $scope.eventname='';//empties input field
+                    $scope.eventdate='';//empties input field
+                    $scope.eventtype='';//empties input field
+                    window.location.href = "#/events";
+                }); //promise
+            } //editEvent
 
-}]); //myApp.controller
+        }]); //myApp.controller
